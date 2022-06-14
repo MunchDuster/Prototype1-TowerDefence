30c449513ecd1a7330f96e7a7414fb1f3534ef58
MunchDuster <malachaiwilkinsongee@rhs.school.nz>
2022-06-14 17:16:59 +1200

    Adding conveyor belt

---
+++ Assets/Scripts/ConveyorItem.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ConveyorItem : MonoBehaviour
+{
+	public int pointIndex = 0;
+	public float totalDist = 0;
+	
+	
+	public bool isWaiting = false;
+	public int waitIndex = 0;
+}
